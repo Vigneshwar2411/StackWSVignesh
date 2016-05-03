0ce10b26becd04d33da4ce60efec2a1085019626
vigneshwar2411 <vigneshwarr@outlook.com>
2016-05-03 15:56:14 +0530

    Bootstrap Exercise

---
+++ Bootstrap/includes/js/script.js
@@ -0,0 +1,39 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+
+/*$(function() {
+
+
+    $('#alertMe').click(function(e){
+
+      e.preventDefault();
+
+      $('#successAlert').slideDown();
+    });
+
+    $('a.pop').click(function(e){
+      e.preventDefault();
+    });
+
+    $('a.pop').popover();
+
+    $('[rel="tooltip"]').tooltip();
+
+});*/
+
+function myFunction() {
+  $('#successAlert').slideDown();
+}
+
+function myclose(){
+  $('#successAlert').toggle();
+}
